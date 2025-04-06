c6649616149340689e5c3ffd8c451c1f2b2dc7d1
Maycon <mayconfabricio2009@gmail.com>
2025-04-05 21:06:23 -0300

    feat: Programa com funcão

---
+++ dados_com_funcao.py
@@ -0,0 +1,6 @@
+def normalizar_nome(nome: str) -> str:
+    return nome.strip().lower()
+
+nomes = [" Alice ", "BOB", "Carlos"]
+nomes_normalizados = [normalizar_nome(nome) for nome in nomes]
+print(nomes_normalizados)
