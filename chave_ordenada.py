50a080a0d363c45f426ba4ae764843eaed3defdc
Maycon <mayconfabricio2009@gmail.com>
2025-04-06 10:04:48 -0300

    feat: Programa com chave ordenada

---
+++ chave_ordenada.py
@@ -0,0 +1,7 @@
+def chaves_ordenadas(dicionario):
+    return sorted(dicionario.keys())
+
+# Exemplo de uso
+dados = {"banana": 3, "maçã": 5, "laranja": 2}
+resultado = chaves_ordenadas(dados)
+print("Chaves ordenadas:", resultado)
